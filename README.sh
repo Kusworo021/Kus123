1e71549bfcf9f0e35bbe1476fb5d2d9525ada1c3
Kusworo021 <88196944+Kusworo021@users.noreply.github.com>
2021-08-22 23:23:45 +0700

    Update and rename README.md to README.sh

---
+++ README.sh
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Kusworo021
+- 👀 I’m interested in kumpulan
+- 🌱 I’m currently learning kumpulan
+- 💞️ I’m looking to collaborate on kumpulan
+- 📫 How to reach me kumpulan
+
+<!-kumpulan
+Kusworo021/Kusworo021 is a ✨ special ✨ repository because its `README.sh` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
